2bad15009a2665121a2a3ddb0514506c19305601
Fenke Meijer <fenke@live.com>
2025-01-26 11:10:16 +0100

    updated to use base class

--- corebridge/aicorebridge.py
+++ corebridge/aicorebridge.py
@@ -78,30 +78,19 @@ def build_historic_args(data:pd.DataFrame, history:dict|list) -> dict:
 
 
 # %% ../nbs/01_aicorebridge.ipynb 12
-class AICoreModule(AICoreModuleBase): pass
-
-# %% ../nbs/01_aicorebridge.ipynb 13
-@patch
-def __init__(self:AICoreModule, 
+class AICoreModule(AICoreModuleBase):
+    def __init__(self, 
              processor:typing.Callable, # data processing function
              save_dir:str, # path where the module can keep files 
              assets_dir:str,
              *args, **kwargs):
     
-    self.init_time = datetime.datetime.now(datetime.UTC)
-    self.aicorebridge_version = __version__
-    self._init_processor(processor)
-
-    self.init_args = args
-    self.init_kwargs = dict(
-        **kwargs,
-        assets_dir=assets_dir,
-        save_dir=save_dir
-    )
+        super().__init__(save_dir, assets_dir, *args, **kwargs)
+        self._init_processor(processor)
 
 
 
-# %% ../nbs/01_aicorebridge.ipynb 14
+# %% ../nbs/01_aicorebridge.ipynb 13
 @patch
 def _init_processor(
         self:AICoreModule, 
@@ -126,7 +115,7 @@ def _init_processor(
 
 
 
-# %% ../nbs/01_aicorebridge.ipynb 15
+# %% ../nbs/01_aicorebridge.ipynb 14
 # can be overloaded
 @patch
 def call_processor(self:AICoreModule, calldata, **callargs):
@@ -136,7 +125,7 @@ def call_processor(self:AICoreModule, calldata, **callargs):
         return self.processor(**callargs)
 
 
-# %% ../nbs/01_aicorebridge.ipynb 17
+# %% ../nbs/01_aicorebridge.ipynb 16
 @patch
 def infer(self:AICoreModule, data:dict, *_, **kwargs):
     try:
@@ -226,7 +215,7 @@ def infer(self:AICoreModule, data:dict, *_, **kwargs):
         }
 
 
-# %% ../nbs/01_aicorebridge.ipynb 19
+# %% ../nbs/01_aicorebridge.ipynb 18
 # Specialized types for initializing annotated parameters
 # Add types by adding a tuple with the type name and a builder function
 annotated_arg_builders = {
@@ -236,7 +225,7 @@ annotated_arg_builders = {
 }
 
 
-# %% ../nbs/01_aicorebridge.ipynb 20
+# %% ../nbs/01_aicorebridge.ipynb 19
 @patch
 def init_annotated_param(self:AICoreModule, param_name, value):
     """
@@ -263,7 +252,7 @@ def init_annotated_param(self:AICoreModule, param_name, value):
 
  
 
-# %% ../nbs/01_aicorebridge.ipynb 21
+# %% ../nbs/01_aicorebridge.ipynb 20
 @patch
 def get_callargs(self:AICoreModule, kwargs, history):
     "Get arguments for the processor call"
@@ -289,7 +278,7 @@ def get_callargs(self:AICoreModule, kwargs, history):
     }
 
 
-# %% ../nbs/01_aicorebridge.ipynb 24
+# %% ../nbs/01_aicorebridge.ipynb 23
 @patch
 def get_call_data(
         self:AICoreModule, 
